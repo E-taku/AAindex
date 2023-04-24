b2e9d4042b4c7fff57a1f880d0a6ba9ecec6cbbe
E-taku <tkymtk8270@gmail.com>
2023-04-24 23:19:06 +0900

    ✅ テストケースの追加

---
+++ test_aaindex1.py
@@ -0,0 +1,35 @@
+import unittest
+from aaindex1 import AAindex1 as a1
+
+class AAindex1TestCase(unittest.TestCase):
+    def setUp(self):
+        self.testA1 = a1()
+        
+    def test_get_all_aaindex1_data(self):
+        result = self.testA1.get_all_aaindex1_number()
+        self.assertEqual(result, 566)
+
+    def test01_get_all_aaindex1_names(self):
+        result = self.testA1.get_all_aaindex1_names()
+        test_data = "alpha-CH chemical shifts (Andersen et al., 1992)"
+        self.assertIn(test_data, result)
+
+    def test02_get_all_aaindex1_names(self):
+        result = self.testA1.get_all_aaindex1_names()
+        test_data = "Scaled side chain hydrophobicity values (Black-Mould, 1991)"
+        self.assertIn(test_data, result)
+    
+    def test01_get_amino_values(self):
+        result = self.testA1.get_amino_values()
+        test_data = "KARS160122"
+        self.assertIn(test_data, result)
+    
+    def test02_get_amino_values(self):
+        result = self.testA1.get_amino_values()
+        test_data = "KARS160113"
+        self.assertIn(test_data, result)
+    
+
+
+if __name__ == "__main__":
+    unittest.main()
